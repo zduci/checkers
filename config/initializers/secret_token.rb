216a555b611a66d5b343dcaf8c468e50794a10e9
Radu Busuioc <ra.busuioc@gmail.com>
2013-02-23 12:01:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkers::Application.config.secret_token = 'b06b6d87b9a7dc6d3014039ff23eb0380f37b020f81e4673266af98b780bb561a80505920202dbb715c1e11493d45c2a362f996f048ebb7c92ad07895ff57887'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Checkers::Application.config.session_store :cookie_store, key: '_checkers_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Checkers::Application.config.session_store :active_record_store
