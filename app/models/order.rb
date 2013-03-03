34021850746c05392f7f72d7e6b3849a4a0e3c32
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-03 23:05:34 +0000

    Make ItemOrder dependent on Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
   attr_accessible :status, :item_orders, :request_id
-  has_many :item_orders
+  has_many :item_orders, :dependent => :destroy
   validates_presence_of :status 
 end
