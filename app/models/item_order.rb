519d0191ebf301aaa95fa1fdc6fb8e7750c31ef7
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-03 19:45:28 +0000

    Make order and item on ItemOrder accessible

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,5 +1,5 @@
 class ItemOrder < ActiveRecord::Base
-  attr_accessible :quantity
+  attr_accessible :quantity, :order, :item
   belongs_to :item
   belongs_to :order
   validates_presence_of :quantity
