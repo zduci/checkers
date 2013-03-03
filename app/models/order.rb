9c1f3ba6ae90b8cd97e5decdd593b81a54ccd108
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-03 19:37:22 +0000

    Make request_id accessible

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  attr_accessible :status, :item_orders
+  attr_accessible :status, :item_orders, :request_id
   has_many :item_orders
   validates_presence_of :status 
 end
