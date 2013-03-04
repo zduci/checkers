90d9bf7c8f1dc2eba4a8d2058ea2791686eb086d
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-04 22:11:15 +0000

    Display items in basket

--- lib/retrieves_orders.rb
+++ lib/retrieves_orders.rb
@@ -1,10 +1,14 @@
 class RetrievesOrders
   def self.existing_or_create(request_id)
-    order = Order.find_by_request_id(request_id)
+    order = find_existing(request_id)
     unless order
       order = Order.create(:status => 'pending',
                    :request_id => request_id)
     end
     order
+  end
+
+  def self.find_existing(request_id)
+    Order.find_by_request_id(request_id)
   end
 end
