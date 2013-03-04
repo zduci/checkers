0df6b7c30916da87d17f96a95acbfb7fe0972e59
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-04 23:16:56 +0000

    Prices computes cost of orders

--- lib/prices.rb
+++ lib/prices.rb
@@ -2,4 +2,10 @@ class Prices
   def self.for_item_order(item_order)
     item_order.quantity * item_order.item.price
   end
+
+  def self.for_order(order)
+    order.item_orders.inject(0) do |result, item_order| 
+      result += for_item_order(item_order)
+    end
+  end
 end
--- spec/lib/prices_spec.rb
+++ spec/lib/prices_spec.rb
@@ -1,10 +1,19 @@
 require_relative '../../lib/prices'
 
 describe Prices do
-  it 'computes the price of its items' do
+  it "computes the price of an item order's items" do
     item = double(:price => 11)
     item_order = double(:item => item, 
                         :quantity => 4)
     Prices.for_item_order(item_order).should == 44
+  end
+
+  it "computes the price of an order's items" do
+    first_item = stub(:price => 10)
+    second_item = stub(:price => 16)
+    order_items = [stub(:item => first_item, :quantity => 1),
+                   stub(:item => second_item, :quantity => 2)]
+    order = stub(:item_orders => order_items)
+    Prices.for_order(order).should == 42
   end
 end
