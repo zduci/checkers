568ec8d67889735dd59ed66ac52579c84c931ea2
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-05 21:59:56 +0000

    Add order tracker

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Checkers::Application.routes.draw do
-  resources :items, :only => [:index, :show], :path => ''
+  resources :items, :only => [:index, :show], :path => '/menu'
   resources :item_orders, :only => [:create]
+  get '/track' => 'track_order#track', :as => 'tracker'
 end
