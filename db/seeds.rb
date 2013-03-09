ce68f2b46a942e686b191339a4fc23775bebd9b8
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-09 19:16:13 +0000

    Add admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,9 @@
   ["Hawaiian Pizza", 9.99, "Ham + pineapple = <3"],
   ["Cheese Pizza", 9.99, "Very cheesy"],
   ["Soda", 2.99, "Deadly eventually"]].each do |item|
-  Item.find_or_create_by_name_and_price_and_description(item[0], item[1], item[2])
+  Item.find_or_create_by_name_and_price_and_description!(item[0], item[1], item[2])
 end
+
+Admin.find_or_create_by_email!(:email => 'pizzadmin@checkers.co.uk',
+                              :password => 'onetwothree')
+
