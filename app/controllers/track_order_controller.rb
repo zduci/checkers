aa79c7141ad1e74efa2a894399b6954d2182c4c6
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-12 22:36:37 +0000

    Update order status in real time

--- app/controllers/track_order_controller.rb
+++ app/controllers/track_order_controller.rb
@@ -1,8 +1,19 @@
 class TrackOrderController < ApplicationController
+  respond_to :html, :js
+
   def track
     @order = RetrievesOrders.find_existing(params[:session_id])
     unless @order
       redirect_to :root
+    end
+  end
+
+  def status
+    respond_to do |format|
+      format.js do
+        @order = RetrievesOrders.find_existing(params[:session_id])
+        render 'update'
+      end
     end
   end
 end
