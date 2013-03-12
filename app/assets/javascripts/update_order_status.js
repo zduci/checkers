2a4b9ee8dd010a923052c6bdd09242d2dd9a6cf9
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-12 22:27:50 +0000

    Query server periodically

--- app/assets/javascripts/update_order_status.js
+++ app/assets/javascripts/update_order_status.js
@@ -1,3 +1,19 @@
 function update_status(new_status){
   $('#order_status').text(new_status);
 }
+
+function session_id(){
+  return $('#order_status').data('sessionId');
+}
+
+$(document).ready(function(){
+  setInterval(get_status, 30000);
+});
+
+function get_status(){
+  $.ajax({
+    url:  "/track/status/" + session_id(),
+    type: "GET",
+  }).done(function(data){
+  });
+}
