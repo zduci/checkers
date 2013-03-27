509780c326d0bedf48536391d91e329be48c1087
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-27 23:07:17 +0000

    Test orders#update

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -51,4 +51,33 @@ describe OrdersController do
       expect { get :show, :id => id }.to raise_error NoMethodError
     end
   end
+
+  context 'updating an order' do
+    let(:id) { '1337' }
+
+    context 'if logged in' do
+      it 'it renders success template if it can update status' do
+        sign_in FactoryGirl.create(:admin)
+        order = stub
+        status = stub
+        RetrievesOrders.stub(:update_status).with(id, status) { true }
+        RetrievesOrders.stub(:find_existing_by_id).with(id) { order }
+        post :update, :format => :js, :id => id, :status => status
+        response.should render_template("success")
+      end
+
+      it "returns fail if it can't update status" do
+        sign_in FactoryGirl.create(:admin)
+        order = stub
+        status = stub
+        RetrievesOrders.stub(:update_status).with(id, status) { false }
+        post :update, :format => :js, :id => id, :status => status
+        response.body.should == 'fail'
+      end
+    end
+
+    it 'fails if not logged in' do
+      expect { post :update, :id => id }.to raise_error NoMethodError
+    end
+  end
 end
