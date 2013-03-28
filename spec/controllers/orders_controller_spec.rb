c440b98c5c98720ee08bfc014fedc30b31ec3b03
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-28 22:04:48 +0000

    Refactor tests

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -3,8 +3,9 @@ include Devise::TestHelpers
 
 describe OrdersController do
   context 'creats a new item' do
+    let(:order) { stub }
+
     it 'creates a new item' do
-      order = stub
       RetrievesOrders.stub(:place_order) { true }
       RetrievesOrders.stub(:find_existing) { order } 
       get :create
@@ -12,16 +13,19 @@ describe OrdersController do
     end
 
     it 'does not create a new item if it is already created and redirects to items path' do
-      order = stub
       RetrievesOrders.stub(:place_order) { false }
       get :create
       response.should redirect_to(items_path)
     end
   end
 
+  def sign_in_admin
+    sign_in FactoryGirl.create(:admin)
+  end
+
   context 'listing all orders' do
     it 'works if logged in' do
-      sign_in FactoryGirl.create(:admin)
+      sign_in_admin
       orders = stub
       Order.stub(:all) { orders }
       get :index
@@ -33,13 +37,14 @@ describe OrdersController do
     end
   end
 
-  context 'showing an order' do
-    let(:id) { '1337' }
+  let(:id) { '1337' }
+  let(:order) { stub }
+  let(:status) { stub }
+  let(:statuses) { stub }
 
+  context 'showing an order' do
     it 'works if logged in' do
-      sign_in FactoryGirl.create(:admin)
-      order = stub
-      statuses = stub
+      sign_in_admin
       RetrievesOrders.stub(:find_existing_by_id).with(id) { order }
       OrderStatus.stub(:for_select) { statuses }
       get :show, :id => id
@@ -53,13 +58,9 @@ describe OrdersController do
   end
 
   context 'updating an order' do
-    let(:id) { '1337' }
-
     context 'if logged in' do
       it 'it renders success template if it can update status' do
-        sign_in FactoryGirl.create(:admin)
-        order = stub
-        status = stub
+        sign_in_admin
         RetrievesOrders.stub(:update_status).with(id, status) { true }
         RetrievesOrders.stub(:find_existing_by_id).with(id) { order }
         post :update, :format => :js, :id => id, :status => status
@@ -67,9 +68,7 @@ describe OrdersController do
       end
 
       it "returns fail if it can't update status" do
-        sign_in FactoryGirl.create(:admin)
-        order = stub
-        status = stub
+        sign_in_admin
         RetrievesOrders.stub(:update_status).with(id, status) { false }
         post :update, :format => :js, :id => id, :status => status
         response.body.should == 'fail'
