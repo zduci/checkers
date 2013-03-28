9295c33eb691d0dc67dd884c1605ecc3427a4825
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-28 22:13:24 +0000

    Restructure tests

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,49 +2,24 @@ require 'spec_helper'
 include Devise::TestHelpers
 
 describe OrdersController do
-  context 'creats a new item' do
+  context 'when logged in' do
+    before(:each) do
+      sign_in FactoryGirl.create(:admin)
+    end
+
+    let(:id) { '1337' }
     let(:order) { stub }
+    let(:status) { stub }
+    let(:statuses) { stub }
 
-    it 'creates a new item' do
-      RetrievesOrders.stub(:place_order) { true }
-      RetrievesOrders.stub(:find_existing) { order } 
-      get :create
-      assigns[:order].should == order
-    end
-
-    it 'does not create a new item if it is already created and redirects to items path' do
-      RetrievesOrders.stub(:place_order) { false }
-      get :create
-      response.should redirect_to(items_path)
-    end
-  end
-
-  def sign_in_admin
-    sign_in FactoryGirl.create(:admin)
-  end
-
-  context 'listing all orders' do
-    it 'works if logged in' do
-      sign_in_admin
+    it 'lists all orders' do
       orders = stub
       Order.stub(:all) { orders }
       get :index
       assigns[:orders].should == orders
     end
 
-    it 'fails if not logged in' do
-      expect { get :index }.to raise_error NoMethodError
-    end
-  end
-
-  let(:id) { '1337' }
-  let(:order) { stub }
-  let(:status) { stub }
-  let(:statuses) { stub }
-
-  context 'showing an order' do
-    it 'works if logged in' do
-      sign_in_admin
+    it 'shows an individual order' do
       RetrievesOrders.stub(:find_existing_by_id).with(id) { order }
       OrderStatus.stub(:for_select) { statuses }
       get :show, :id => id
@@ -52,31 +27,49 @@ describe OrdersController do
       assigns[:statuses].should == statuses
     end
 
-    it 'fails if not logged in' do
-      expect { get :show, :id => id }.to raise_error NoMethodError
+    context 'updating an order' do
+        it 'renders success template if it can update status' do
+          RetrievesOrders.stub(:update_status).with(id, status) { true }
+          RetrievesOrders.stub(:find_existing_by_id).with(id) { order }
+          post :update, :format => :js, :id => id, :status => status
+          response.should render_template("success")
+        end
+
+        it "returns fail if it can't update status" do
+          RetrievesOrders.stub(:update_status).with(id, status) { false }
+          post :update, :format => :js, :id => id, :status => status
+          response.body.should == 'fail'
+        end
     end
   end
 
-  context 'updating an order' do
-    context 'if logged in' do
-      it 'it renders success template if it can update status' do
-        sign_in_admin
-        RetrievesOrders.stub(:update_status).with(id, status) { true }
-        RetrievesOrders.stub(:find_existing_by_id).with(id) { order }
-        post :update, :format => :js, :id => id, :status => status
-        response.should render_template("success")
-      end
+  context 'when not logged in' do
+    it 'does not list orders' do
+      expect { get :index }.to raise_error Exception
+    end
 
-      it "returns fail if it can't update status" do
-        sign_in_admin
-        RetrievesOrders.stub(:update_status).with(id, status) { false }
-        post :update, :format => :js, :id => id, :status => status
-        response.body.should == 'fail'
-      end
+    it 'does not show individual orders' do
+      expect { get :show, :id => id }.to raise_error Exception
     end
 
     it 'fails if not logged in' do
-      expect { post :update, :id => id }.to raise_error NoMethodError
+      expect { post :update, :id => id }.to raise_error Exception
+    end
+
+    context 'creates a new item' do
+      it 'creates a new item' do
+        order = stub
+        RetrievesOrders.stub(:place_order) { true }
+        RetrievesOrders.stub(:find_existing) { order } 
+        get :create
+        assigns[:order].should == order
+      end
+
+      it 'does not create a new item if it is already created and redirects to items path' do
+        RetrievesOrders.stub(:place_order) { false }
+        get :create
+        response.should redirect_to(items_path)
+      end
     end
   end
 end
