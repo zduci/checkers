78e22a0bb3665fed71fbc5758aab162d8d110150
Radu Busuioc <ra.busuioc@gmail.com>
2013-04-16 23:12:57 +0100

    Test adds items to basket

--- spec/features/place_order_spec.rb
+++ spec/features/place_order_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
+
 describe "placing an order", :type => :feature do
+    let(:item) { FactoryGirl.create(:item) }
+
     before(:each) do
-      Item.stub(:all) { [FactoryGirl.create(:item)] }
+      Item.stub(:all) { [ item ] }
     end
 
     it 'displays items' do
@@ -10,5 +13,16 @@ describe "placing an order", :type => :feature do
       page.should have_content 'Cheese Pizza'
       page.should have_content 'Very cheesy'
       page.should have_content '9.99'
+    end
+
+    it "adds items to the basket" do
+      visit '/menu'
+
+      click_button 'Select quantity'
+      puts page.body
+      select '10', :from => 'quantity'
+
+      click_button 'Add to order'
+      page.should have_content 'Total Price 99.9'
     end
 end
