6751f470b5fad60f3c76cecdcc5fe7862a6171d9
Radu Busuioc <ra.busuioc@gmail.com>
2013-04-29 21:02:53 +0100

    Move helper methods to place_order_helper

--- spec/features/place_order_helper.rb
+++ spec/features/place_order_helper.rb
@@ -12,4 +12,14 @@ module PlaceOrderHelper
     def place_order
       click_button 'Place order'
     end
+
+    def place_order_log_in_and_visit_orders
+      visit_menu
+      select_quantity
+      place_order
+      order_url = current_url
+      log_in
+      visit_orders
+      order_url
+    end
 end
